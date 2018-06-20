2ae76b3e807da23745f9354347ffeff256b6c6d5
sxw <1161113846@qq.com>
2018-06-20 09:44:42 +0800

    sxw

---
+++ mvc_c/Controller/Home/Message.php
@@ -0,0 +1,24 @@
+<?php 
+namespace Controller;
+
+class message extends Controller
+{
+	public function index()
+	{
+		print_r(111);
+		// 查库 select
+	}
+	public function add()
+	{
+		// insert
+	}
+	public function update()
+	{
+		// update
+	}
+	public function delete()
+	{
+		// delete
+	}
+
+}
